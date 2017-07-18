cb67989bab2ffd295667b10e74bc204c6101cee9
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-18 14:20:48 -0700

    Rearticulate splines

--- Bento/config/routes.rb
+++ Bento/config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
   end
 
-  namespace :api do
-    get 'users/new'
-    get 'users/create'
-  end
-
-
+  root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
