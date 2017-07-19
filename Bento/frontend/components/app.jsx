1d9d1269240adfc440e0e101ead0226474ba7116
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-18 21:15:06 -0700

    Begin rendering-component tackling

---
+++ Bento/frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+
+const App = () => (
+  <div className="app">
+    <h1>Hi from App</h1>
+  </div>
+);
+
+export default App;
--- Bento/frontend/components/root.jsx
+++ Bento/frontend/components/root.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, Route } from 'react-router-dom';
 
-// import App from './app';
+import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <HashRouter>
-      <h1>HELLO</h1>
+      <App />
     </HashRouter>
   </Provider>
 );
