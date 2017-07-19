6bca53cba0c11fd6c5c6b2797e2d2f875c6d8429
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-18 21:18:50 -0700

    Add splash component and container, add testing suite for state

--- Bento/frontend/entry.jsx
+++ Bento/frontend/entry.jsx
@@ -8,6 +8,11 @@ import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
+  //Remove after testing is complete
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  //Remove after testing is complete
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root)
 });
