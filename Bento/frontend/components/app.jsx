d2446404ef4b1155d9e02d771e45288285d41ccf
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-19 23:46:48 -0700

    Add logo to nav bar, begin work on important animations

--- Bento/frontend/components/app.jsx
+++ Bento/frontend/components/app.jsx
@@ -7,7 +7,13 @@ import SessionFormContainer from './session_form_component/session_form_containe
 const App = () => (
   <div className="app">
     <div className="header-flex-container">
-      <div className="spacer"></div>
+      <div className="logo-spacer">
+
+        <div className="logo-container">
+
+          <img src="http://res.cloudinary.com/atomc/image/upload/v1500531260/Bento-Logo_fjv1os.png"></img>
+        </div>
+      </div>
 
       <div className="spacer"></div>
 
@@ -28,7 +34,7 @@ const App = () => (
       </div>
     </div>
     <div className="session-form-containers">
-      
+
       <Route
         path="/login"
         component={ SessionFormContainer }
