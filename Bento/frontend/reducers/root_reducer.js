a3a09d73b2bdd105b3cd003ba1bb8763d14bf6c3
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-20 12:07:09 -0700

    Begin work on team reduction

--- Bento/frontend/reducers/root_reducer.js
+++ Bento/frontend/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import SessionReducer from './session_reducer';
+import teamsReducer from './session_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  teams: teamsReducer
 });
 
 export default RootReducer;
