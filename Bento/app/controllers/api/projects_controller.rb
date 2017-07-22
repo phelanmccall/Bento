833fd650c6941d4b3304ff3532abbfa9886a39a1
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 19:03:38 -0700

    Content self with project controller draft

--- Bento/app/controllers/api/projects_controller.rb
+++ Bento/app/controllers/api/projects_controller.rb
@@ -10,16 +10,28 @@ class Api::ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.manager_id = current_user.id
+    if @project.save
+      render 'api/projects/show'
+    else
+      render json: @project.errors.full_messages, status: 422
+    end
   end
 
   def update
     @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      render 'api/projects/show'
+    else
+      render json: @project.errors.full_messages, status: 422
+    end
 
   end
 
   def destroy
     @project = Project.find(params[:id])
-
+    @project.destroy
+    render 'api/projects/show'
   end
 
   private
