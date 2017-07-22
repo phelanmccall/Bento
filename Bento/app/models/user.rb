b54b6a21b666b099a336534eb26482115414b81d
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 20:19:39 -0700

    Fix single character issue that caused a failed migration

--- Bento/app/models/user.rb
+++ Bento/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 	after_initialize :ensure_session_token
 	before_validation :ensure_session_token_uniqueness
 
-	has_many: projects
+	has_many :projects
 
 	def password= password
 		self.password_digest = BCrypt::Password.create(password)
--- Bento/db/schema.rb
+++ Bento/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718190044) do
+ActiveRecord::Schema.define(version: 20170722011607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "manager"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
