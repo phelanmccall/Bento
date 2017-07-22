5bf3a1d2553e47b227e04502162d3cb39ddb46ba
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 23:54:26 -0700

    Completely rework heirarchical scss structure and manage not to break a single thing

--- Bento/frontend/components/app.jsx
+++ Bento/frontend/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
+import Head from './head'
 import NavContainer from './navbar/navbar_container';
 import SplashContainer from './splash/splash_container';
 import CreateProjectContainer from './projects/create_project_container';
@@ -9,32 +10,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div className="app">
-    <div className="header-flex-container">
-      <div className="logo-spacer">
-
-        <div className="logo-container">
-
-          <Link to="/login"><img src="http://res.cloudinary.com/atomc/image/upload/v1500531260/Bento-Logo_fjv1os.png"></img></Link>
-        </div>
-      </div>
-
-      <div className="spacer"></div>
-
-      <div className="header-container">
-        <header>
-            <Link to="/login"><h1 className="Bento">Bento</h1></Link>
-        </header>
-      </div>
-
-      <div className="spacer"></div>
-
-      <div className="splash-container">
-          <div className="splash-spacer">
-            <NavContainer />
-          </div>
-          <div className="spacer"></div>
-      </div>
-    </div>
+    <Head />
     <div className="session-form-containers">
       <CreateProjectContainer />
       <switch>
