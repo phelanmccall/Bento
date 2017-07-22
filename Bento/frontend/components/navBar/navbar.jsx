3f5dcddd068cd7b9db547eb6dd02dec4ee01f323
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 18:16:40 -0700

    Instantiate projects mvp

--- Bento/frontend/components/navBar/navbar.jsx
+++ Bento/frontend/components/navBar/navbar.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const navSessionLinks = () => (
 
     <nav className="nav-login-signup-flex-container">
       <div className="spacer"></div>
-      <NavLink
-        activeClassName="reactive"
+      <Link
         className="nav-login-button"
         to="/login">
         log in<div className="nest"> { "" }</div>
-      </NavLink>
+    </Link>
       <div className="spacer special-space"></div>
-      <NavLink
-        activeClassName="reactive"
+      <Link
         className="nav-signup-button"
         to="/signup">
         sign up<div className="nest"> { "" }</div>
-      </NavLink>
+    </Link>
       <div className="spacer"></div>
-
-    </nav>
+  </nav>
 
 );
 
