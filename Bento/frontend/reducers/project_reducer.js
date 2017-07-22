543886fd69ad228d30b520975034434eba05c601
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 20:39:24 -0700

    Go hard on projects

---
+++ Bento/frontend/reducers/project_reducer.js
@@ -0,0 +1,32 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_PROJECT,
+  RECEIVE_ALL_PROJECTS,
+  REMOVE_PROJECT
+} from '../actions/project_actions';
+
+const startState = Object.freeze({
+  title: "",
+  manager_id: ""
+});
+
+const PostReducer = (state = startState, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_PROJECT:
+      let newPost = {[action.project.id]: action.project};
+      return merge({}, state, newPost);
+    case RECEIVE_ALL_PROJECTS:
+      return merge({}, action.projects);
+    case REMOVE_PROJECT:
+      let nextState = merge({}, state);
+      delete nextState[action.project.id];
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default PostReducer;
