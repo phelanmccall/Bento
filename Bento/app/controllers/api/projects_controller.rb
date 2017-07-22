2f70bad940f0bfe45459ff7a94730958cd880406
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-21 18:31:30 -0700

    Forge project controller actions

--- Bento/app/controllers/api/projects_controller.rb
+++ Bento/app/controllers/api/projects_controller.rb
@@ -1,2 +1,24 @@
 class Api::ProjectsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+  end
+
 end
