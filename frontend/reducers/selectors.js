e33e7a3afab4810e2de344dae08f5601c115fcfc
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-24 23:22:44 -0700

    Pour mould for Tasks

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -3,3 +3,8 @@ import { values } from 'lodash';
 export const SelectorAllProjects = projects => {
   return values(projects);
 };
+
+
+export const SelectorAllTasks = tasks => {
+  return values(tasks);
+};
