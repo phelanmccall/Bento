b2739b358408f4f33f31faace8da61cfc30103b5
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-28 02:19:27 -0700

    Add left bar for teams, further style

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,43 @@
+import * as APIUtil from '../util/user_api_util';
+
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const RECEIVE_USER =
+'RECEIVE_USER';
+
+export const receiveUsers = (users) => {
+  return {
+    type: RECEIVE_USERS,
+    users
+  }
+}
+
+export const receiveUser = (user) => {
+  return {
+    type: RECEIVE_USER,
+    user
+  }
+}
+
+export const fetchAllUsers = () => (dispatch) => {
+  return APIUtil.fetchAllUsers().then(
+    resp => dispatch(receiveUsers(resp))
+  )
+}
+
+export const createUser = (user) => (dispatch) => {
+  return APIUtil.createUser(user).then(
+    resp => dispatch(receiveUser(resp))
+  )
+}
+
+export const fetchUser = (user) => (dispatch) => {
+  return APIUtil.fetchUser(user).then(
+    resp => dispatch(receiveUser(resp))
+  )
+}
+
+export const updateUser = (user) => (dispatch) => {
+  return APIUtil.updateUser(user).then(
+    resp => dispatch(receiveUser(resp))
+  )
+}
---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,33 @@
+export const fetchAllUsers = (user_id) => {
+  console.log("User api util user id: " + user_id);
+  return $.ajax({
+    method: 'GET',
+    url: '/api/users',
+    { user }
+  })
+}
+
+export const fetchUser = (id) =>
+{
+  return $.ajax({
+    method: 'GET',
+    url: `/api/users/${id}`
+  })
+}
+
+export const createUser = (user) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/users',
+    { user }
+  })
+}
+
+export const updateUser = (user) =>
+{
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/users/${user.id}`,
+    { user }
+  })
+}
