26fc57d4b0e78fe654f18804058d9feaac5fc7b6
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-09-18 00:00:00 -0700

    Array transformation successful

--- frontend/reducers/task_reducer.js
+++ frontend/reducers/task_reducer.js
@@ -6,7 +6,7 @@ import {
 } from '../actions/task_actions';
 import { CLEAR_STORE } from '../actions/session_actions';
 
-const TaskReducer = (state = {}, action) => {
+const TaskReducer = (state = [], action) => {
   Object.freeze(state);
 
     // maybe structure this slice of state as an array instead of an object.
@@ -14,15 +14,15 @@ const TaskReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_TASK:
     let newTask = {[action.task.id]: action.task };
-    return merge({}, state, newTask);
+    return merge([], state, newTask);
     case RECEIVE_ALL_TASKS:
       return action.tasks
     case REMOVE_TASK:
-      let nextState = merge({}, state);
+      let nextState = merge([], state);
       delete nextState[action.task.id];
       return nextState;
     case CLEAR_STORE:
-      return {};
+      return [];
     default:
       return state;
   }
