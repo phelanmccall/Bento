6e1658efa657b12c704b9f995b566724fac498b2
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-09-19 18:25:23 -0700

    Added todo for restructuring task state to a much more expressive object notation

--- frontend/reducers/task_reducer.js
+++ frontend/reducers/task_reducer.js
@@ -13,6 +13,10 @@ const TaskReducer = (state = [], action) => {
 
     // sorta works but obvs broke some stuff. fix in morning.
 
+    // Need to map this differently, just had an idea. Tasks should be taken by matches to team (so we have everything that is currently on screen. NB we have all Teams on screen, all Projects on screen (just for one team at a time) and then all Tasks. Should just have all tasks by team, and then nest them in THEIR OWN slice of state. TODO nest as such:)
+
+    // tasks: {proj1: [{t1}, {t2}, {t3}]} (only from current team. selector for this.)
+
   switch (action.type) {
     case RECEIVE_TASK:
     let newTask = {[action.task.id]: action.task };
